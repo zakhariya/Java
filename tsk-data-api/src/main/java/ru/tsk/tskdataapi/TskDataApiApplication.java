6b0a1ad4d920f2f79e9dd93bcf9e8737f0e906c6
zakhar <alexander.zakhariya@gmail.com>
2022-02-06 16:24:07 +0200

    swagger added

--- tsk-data-api/src/main/java/ru/tsk/tskdataapi/TskDataApiApplication.java
+++ tsk-data-api/src/main/java/ru/tsk/tskdataapi/TskDataApiApplication.java
@@ -2,8 +2,10 @@ package ru.tsk.tskdataapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class TskDataApiApplication {
 
 	public static void main(String[] args) {
