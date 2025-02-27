d446a8ab5aaca732b26c6d65687b4c1a7fbb162d
zakhariya <alexander.zakhariya@gmail.com>
2025-02-27 13:22:03 +0200

    Constants modified

--- examples/src/main/java/ua/od/zakhariya/system/Constants.java
+++ examples/src/main/java/ua/od/zakhariya/system/Constants.java
@@ -5,8 +5,8 @@ import java.io.File;
 
 public class Constants {
     public static final String OS = System.getProperty("os.name");
-    public static final String PROGRAM_PATH = System.getProperty("user.dir");  //directory where java was run from, where you started
-    //the JVM (if not on console, it will be path to program)
+    public static final String PROGRAM_PATH = System.getProperty("user.dir");  //directory where java was run from,
+    // where you started JVM (if not on console, it will be path to program)
     public static final String USER_NAME = System.getProperty("user.name"); //System.getenv("USER")
     public static final String USER_DOCUMENTS = System.getProperty("user.home"); // user`s home directory (C:\Users\Саня Z, /home/worker)
     public static final String EXECUTED_FILE_PATH = System.getProperty("java.class.path"); // real path and file name of application
@@ -14,7 +14,6 @@ public class Constants {
     public static final String USER_DOWNLOADS = Constants.USER_DOCUMENTS + Constants.FILE_SEPARATOR + "Downloads" + Constants.FILE_SEPARATOR;
     public static final String PATH_SEPARATOR = System.getProperty("path.separator"); // separator for list of file paths ";" on win, or ":" on unix
     public static final String LINE_SEPARATOR = System.lineSeparator(); // \n on win, or \r\n on unix
-    public static final String APPLICATION_NAME = "Трансфер данных";
 //    public static final String COMPUTER_NAME = InetAddress.getLocalHost().getHostName();
 
 //    public static final ImageIcon ICON_LOGO =  new ImageIcon(Constants.class.getResource("logo.png"));
