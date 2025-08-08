1753a56a9bc0244184f2560590330be8256024b6
zakhariya <alexander.zakhariya@gmail.com>
2025-08-08 15:52:08 +0300

    example sql email values changed

--- mailer/src/test/resources/data.sql
+++ mailer/src/test/resources/data.sql
@@ -1,4 +1,4 @@
 TRUNCATE polygraphy_email_list;
-INSERT INTO polygraphy_email_list (`name`, `company`, `email`) VALUES ('Александр', 'LPR.UA', 'admin@lpr.ua');
-INSERT INTO polygraphy_email_list (`email`) VALUES ('alexander.zakhariya@gmail.com');
-INSERT INTO polygraphy_email_list (`name`, `email`) VALUES ('Alexander', 'master-kap@ukr.net');
+INSERT INTO polygraphy_email_list (`name`, `company`, `email`) VALUES ('Александр', 'LPR.UA', 'admin@domain.com');
+INSERT INTO polygraphy_email_list (`email`) VALUES ('some.mail@gmail.com');
+INSERT INTO polygraphy_email_list (`name`, `email`) VALUES ('Alexander', 'some.mail@domain.com');
